062772ee8144d901a25d156a1469f97cfd94cd84
João Ricardo Viana <joaorviana98@gmail.com>
2023-07-14 13:59:34 -0300

    req10

--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -2,14 +2,18 @@ import { compareSync } from 'bcryptjs';
 import { tokensign } from './jwt';
 import UserModel from '../database/models/UsersModel';
 
-const invalid = 'All fields must be filled';
+const invalid = 'Invalid email or password';
 
 export default class LoginService {
   private usermodel = UserModel;
 
   public async validateUser(user:any) {
     if (!user.email || !user.password) {
-      return { type: 400, message: invalid };
+      return { type: 400, message: 'All fields must be filled' };
+    }
+    const re = /\S+@\S+\.\S+/;
+    if (!re.test(user.email) || user.password.length < 6) {
+      return { type: 401, message: invalid };
     }
     const { type, message } = await this.validatePassword(user.email, user.password);
 
@@ -19,10 +23,10 @@ export default class LoginService {
   public async validatePassword(email:string, password:string):Promise<any> {
     const valUser = await this.usermodel.findOne({ where: { email } });
     if (!valUser) {
-      return { type: 400, message: invalid };
+      return { type: 401, message: invalid };
     }
     if (!compareSync(password, valUser.password)) {
-      return { type: 400, message: invalid };
+      return { type: 401, message: invalid };
     }
     const payload = { id: valUser.id, username: valUser.userName };
     const token = tokensign(payload);
