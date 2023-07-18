7bcebac68ed92dbc16203f9cf25c239aebdb5d45
João Ricardo Viana <joaorviana98@gmail.com>
2023-07-18 15:04:26 -0300

    testes

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -3,43 +3,72 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
-import { app } from '../app';
-import Example from '../database/models/ExampleModel';
-
-import { Response } from 'superagent';
+import {app} from '../app';
+import {login,teams,home,leaderBoard,postMatch, patchMatch} from './mocks';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
-
-  // let chaiHttpResponse: Response;
-
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
+describe('POST /login ', async function () { 
+  beforeEach(function () { sinon.restore(); });
+    it('Login ', async function () {
+      // Arrange
+      // Act
+      const {status,body} = await chai.request(app).post('/login').send(login);
+      // Assert
+      expect(status).to.be.deep.equal(200);
+      expect(body).to.have.key('token');
+    });
+    it('role', async function () {
+      // Arrange
+      // Act
+      const {body:{token}} = await chai.request(app).post('/login').send(login);
+      const {status,body} = await chai.request(app).get('/login/role').set('authorization',`Bearer ${token}`).send();
+      // Assert
+      expect(status).to.be.deep.equal(200);
+      expect(body).to.have.key('role');
+    });
+    it('teams',async function(){
+      const {status, body} = await chai.request(app).get('/teams').send();
+      // Assert
+      expect(status).to.be.deep.equal(200);
+      expect(body).to.be.deep.equal(teams);
+    })
+    it('home',async function(){
+      const {status,body} = await chai.request(app).get('/leaderboard/home').send();
+      // Assert
+      expect(status).to.be.deep.equal(200);
+      expect(body).to.be.deep.equal(home);
+    })
+    it('leaderboard',async function(){
+      const {status,body} = await chai.request(app).get('/leaderboard').send();
+      // Assert
+      expect(status).to.be.deep.equal(200);
+      expect(body).to.be.deep.equal(leaderBoard);
+    })
+    it('get matches', async function () {
+      // Arrange
+      // Act
+      const {body:{token}} = await chai.request(app).post('/login').send(login);
+      const {status} = await chai.request(app).get('/matches').set('authorization',`Bearer ${token}`).send();
+      // Assert
+      expect(status).to.be.deep.equal(200);
+    });
+    it('post matches', async function () {
+      // Arrange
+      // Act
+      const {body:{token}} = await chai.request(app).post('/login').send(login);
+      const {status} = await chai.request(app).post('/matches').set('authorization',`Bearer ${token}`).send(postMatch);
+      // Assert
+      expect(status).to.be.deep.equal(201);
+    });
+    it('patch matches', async function () {
+      // Arrange
+      // Act
+      const {body:{token}} = await chai.request(app).post('/login').send(login);
+      const {status} = await chai.request(app).patch('/matches/5').set('authorization',`Bearer ${token}`).send(patchMatch);
+      // Assert
+      expect(status).to.be.deep.equal(200);
+    });
 });
